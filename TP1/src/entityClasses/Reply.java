c3b0da4da316bc8ca90123c843cb3497655dac36
Fauzan <137512085+fauzanamaan@users.noreply.github.com>
2026-03-18 13:43:11 -0700

    Update Reply.java

--- TP1/src/entityClasses/Reply.java
+++ TP1/src/entityClasses/Reply.java
@@ -1,1 +1,20 @@
 
+package entityClasses;
+
+public class Reply extends Post{
+	
+	
+	private int parentPostID;
+	
+	public Reply() {
+		super();
+	}
+	
+	public Reply(int parentPostID, String authorUserName, String body) {
+		super(authorUserName, null, body, null, null);
+	}
+	
+	public int getParentPostID() {
+		return parentPostID;
+	}
+}
